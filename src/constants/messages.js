e7f650d01d96b44155c7850d3729c1132128b672
ohgus <anakin1108@naver.com>
2023-12-07 17:23:09 +0900

    feat: 실행결과 메시지 출력 기능 추가

--- src/constants/messages.js
+++ src/constants/messages.js
@@ -4,6 +4,9 @@ const MESSAGE = Object.freeze({
       "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)" + "\n",
     MOVE_COUNT: "시도할 횟수는 몇 회인가요?" + "\n",
   },
+  PRINT: {
+    EXECUTION_RESULT: "\n" + "실행결과",
+  },
 });
 
 export default MESSAGE;
--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,6 +1,11 @@
 import { Console } from "@woowacourse/mission-utils";
+import MESSAGE from "../constants/messages.js";
 
 class OutputView {
+  static printExecutionResult() {
+    Console.print(MESSAGE.PRINT.EXECUTION_RESULT);
+  }
+
   static printIntermediateResults(table) {
     for (let car in table) {
       Console.print(`${car} : ${table[car]}`);
